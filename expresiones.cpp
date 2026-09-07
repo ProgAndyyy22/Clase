227951b4bf1378e4ff89aba8e9f26ccf1f5f17af
ProgAndyyy22 <asaraque@estud.usfq.edu.ec>
2026-09-07 12:23:57 -0500

    Add files via upload

---
+++ expresiones.cpp
@@ -0,0 +1,40 @@
+// Autor: Pablo Yepez
+// Fecha: 3 de septiembre de 2026
+// Descripcion: Exporesiones
+
+#include <iostream>
+using namespace std;
+
+int main( ) {
+	cout << "Expresiones" << endl;
+	
+	int a, b, c, suma;
+	int total;
+	double f, d, puntoflotante;
+	
+	f = 3.876;
+	d = 2.5 + 3 + 4.1;
+	puntoflotante = 2 * 5 / 7;
+	
+	cout << "f:" << f << endl;
+	cout << "d:" << d << endl;
+	cout << "puntoflotante:" << puntoflotante << endl;
+	
+	
+	/*
+	total = 5;
+	cout << "total: " << total << endl;
+	
+	suma = 5 + 3.99999999999;
+	a = 5 - 7;
+	b = 8 + 4 * 5-7 * 6;
+	c = (8+4) * (5-7) * 6;
+	
+	cout << "suma:" << suma << endl;
+	cout << "a:" << a << endl;
+	cout << "b:" << b << endl;
+	cout << "c:" << c << endl;
+	*/
+	
+	return 0;
+}
