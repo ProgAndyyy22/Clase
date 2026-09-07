0e6e7b9fedc5ada1914a87d884d05f5399a4321f
ProgAndyyy22 <asaraque@estud.usfq.edu.ec>
2026-09-07 12:06:53 -0500

    Add files via upload

---
+++ MiPrimerPrograma.cpp
@@ -0,0 +1,24 @@
+// Autor: Pablo Yepez
+// Fecha: 3 de septiembre de 2026
+// Descripcion: Mi primer programa
+
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or 
+add your own getch, 
+system("pause") 
+or input loop */
+
+int main( ) {
+	
+	/* Este es mi primer programa de C++.
+	Estoy muy contento porque ya puedop programas. */
+	
+	cout << "Pablo Yepez" << endl << "text " << endl;
+	cout << "USFQ";
+	cout << endl; // Este sirve para saltar una linea
+	
+	system("pause");
+	return 0;
+}
