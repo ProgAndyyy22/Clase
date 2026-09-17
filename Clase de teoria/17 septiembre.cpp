306b83acd06580a5895c12a774f896c7f1c9cbc9
ProgAndyyy22 <asaraque@estud.usfq.edu.ec>
2026-09-17 12:31:20 +0000

    clase

--- Clase de teoria/17 septiembre.cpp
+++ Clase de teoria/17 septiembre.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    cout << "While loop example" << endl;
+    int a{1}, b{0};
+    while (a < 5) 
+    {
+        cout << a<< " ";
+        a+=1;
+    }
+return 0;
+}
