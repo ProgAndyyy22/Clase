36b4cce3f873ca8b7df89195a06dc556a758b56f
ProgAndyyy22 <asaraque@estud.usfq.edu.ec>
2026-09-17 13:14:27 +0000

    clase

--- Clase de teoria/17 septiembre.cpp
+++ Clase de teoria/17 septiembre.cpp
@@ -4,10 +4,31 @@ using namespace std;
 int main() {
     cout << "While loop example" << endl;
     int a{1}, b{0};
-    while (a < 5) 
+    /*while (a <= 100) 
     {
-        cout << a<< " ";
-        a+=1;
+        if(a %3 == 0 || a %5 == 0){
+            cout << a << " ";
+        }
+
+        a+=2;
+    }*/
+    int  n{1};
+    long long suma{0};
+    int suma_p{0}, suma_i{0};
+    while(n <=100)
+    {  // cout << n << " ";
+        if(n % 2 == 0){
+            suma_p += n;
+        }
+        else{
+            suma_i += n;
+        }
+        suma += n;
+        n++;
+        
     }
-return 0;
+    cout << "Suma: " << suma << endl;
+    cout << "Suma pares: " << suma_p << endl;
+    cout << "Suma impares: " << suma_i << endl;
+    return 0;
 }
