ef17403a45df59c4c0ee1a645af1183b3bf5cde3
ProgAndyyy22 <asaraque@estud.usfq.edu.ec>
2026-09-23 03:25:33 +0000

    ej4 avance

--- Deber2Teoria/Deber3Teoria/ejercicio4.cpp
+++ Deber2Teoria/Deber3Teoria/ejercicio4.cpp
@@ -32,6 +32,11 @@ bool DosAncianos= false;
 bool VanAlCine=true;
 if( (esAnciano1 && esAnciano2)||(esAnciano1 && esAnciano3)||(esAnciano2 && esAnciano2) && !(esAnciano1&&esAnciano2&&esAnciano3)){ DosAncianos= true;}
 float BoletosAncianos{0}, BoletosAdultos{0}, BoletosMenores{0};
+if(esAnciano1){BoletosAncianos+=1;}
+if(esAnciano2){BoletosAncianos+=1;}
+if(esAnciano3){BoletosAncianos+=1;}
+
+if((edad1<8)|(edad2<8)|(edad3<8)){cout << "Hay niños menores de 8 años, la familia cancela ir al cine"<<endl;}
 
 if (EntraPersona1Sala && EntraPersona2Sala && EntraPersona3Sala ) {
 cout << "Las tres personas solo pueden ir a la sala de entretenimiento (son menores de edad), no tiene sentido ir al cine." << endl;
@@ -44,14 +49,11 @@ else if (EntraPersona1Sala || EntraPersona2Sala || EntraPersona3Sala) {
 cout << "Hay un menor de edad, y un adulto tiene que acompañarlos a la sala de entretenimiento." << endl;
     BoletosMenores=1;
     BoletosAdultos=0;
-    if (DosAncianos){cout<< "Hay dos mayores de edad con prioridad para ver la pelicula, y un menor sin cuidar, el menor no va a entrar"<< endl;
-    BoletosMenores=0; BoletosAncianos=2;}
-    else if (esAnciano1){cout << "La primera persona (mayor de 65) tiene prioridad de ver la pelicula. "<< endl;
-    BoletosMenores=2; BoletosAncianos=1;}
-    else if (esAnciano2){cout << "La segunda persona (mayor de 65) tiene prioridad de ver la pelicula. "<<endl;
-    BoletosMenores=2; BoletosAncianos=1;}
-    else if (esAnciano3){cout << "La tercera persona (mayor de 65) tiene prioridad de ver la pelicula. "<<endl; 
-    BoletosMenores=2; BoletosAncianos=1;}
+    if (EntraPersona1Sala){
+
+        if (edad2> edad3){cout<< "La tercera persona va a acompañar al menor de edad"<< endl;}
+        else{cout<<"La segunda persona va a acompañar al menor de edad"<<endl;}}
+
     else{cout << "Una de las dos personas adultas debe ir obligatoriamente con el menor de edad (dos boletos a la sala de entretenimiento) "<< endl;
         BoletosMenores=2;
         BoletosAdultos=1;}
@@ -63,7 +65,7 @@ if(esAnciano3){BoletosAncianos+=1;}else{BoletosAdultos+=1;};
 }
 
 
-cout << "\n Van a entrar: "<< BoletosAdultos+BoletosAncianos+BoletosMenores<< endl;
+cout << "\nVan a entrar: "<< BoletosAdultos+BoletosAncianos+BoletosMenores<< endl;
 cout << "Van a la sala de entreteniemiento: "<< BoletosMenores<< endl;
 cout << "Van a la sala de cine: "<< BoletosAncianos+BoletosAdultos<< endl;
     //
