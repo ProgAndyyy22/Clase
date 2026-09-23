11446c53b255e8a573bc35ad2bba9ab8bc63b53f
ProgAndyyy22 <asaraque@estud.usfq.edu.ec>
2026-09-23 11:35:56 +0000

    ej4

--- Deber2Teoria/Deber3Teoria/ejercicio4.cpp
+++ Deber2Teoria/Deber3Teoria/ejercicio4.cpp
@@ -21,9 +21,9 @@ bool EntraPersona1Sala = (edad1 >= 8 && edad1 <18);
 bool EntraPersona2Sala = (edad2 >= 8 && edad2 <18);
 bool EntraPersona3Sala = (edad3 >= 8 && edad3 <18);
 
-bool EntraCine1 = (edad1 >= 18);
-bool EntraCine2 = (edad2 >= 18);
-bool EntraCine3 = (edad3 >= 18);
+bool EntraCine1 = ((edad1 <65) &&(edad1 >= 18));
+bool EntraCine2 = ((edad2 <65)&&(edad2 >= 18));
+bool EntraCine3 = ((edad3<65)&&(edad3 >= 18));
 
 bool esAnciano1 = (edad1 >= 65);
 bool esAnciano2 = (edad2 >= 65);
@@ -35,10 +35,14 @@ float BoletosAncianos{0}, BoletosAdultos{0}, BoletosMenores{0};
 if(esAnciano1){BoletosAncianos+=1;}
 if(esAnciano2){BoletosAncianos+=1;}
 if(esAnciano3){BoletosAncianos+=1;}
+if(EntraCine1){BoletosAdultos+=1;}
+if(EntraCine2){BoletosAdultos+=1;}
+if(EntraCine3){BoletosAdultos+=1;}
+
 
 if((edad1<8)|(edad2<8)|(edad3<8)){cout << "Hay niños menores de 8 años, la familia cancela ir al cine"<<endl;}
 
-if (EntraPersona1Sala && EntraPersona2Sala && EntraPersona3Sala ) {
+else if (EntraPersona1Sala && EntraPersona2Sala && EntraPersona3Sala ) {
 cout << "Las tres personas solo pueden ir a la sala de entretenimiento (son menores de edad), no tiene sentido ir al cine." << endl;
 VanAlCine=false;}
 else if ((EntraPersona1Sala && EntraPersona2Sala)||(EntraPersona1Sala && EntraPersona3Sala)||(EntraPersona2Sala && EntraPersona3Sala)) {
@@ -47,21 +51,21 @@ VanAlCine=false;}
 else if (EntraPersona1Sala || EntraPersona2Sala || EntraPersona3Sala) {
 
 cout << "Hay un menor de edad, y un adulto tiene que acompañarlos a la sala de entretenimiento." << endl;
-    BoletosMenores=1;
+    BoletosMenores=2;
+    BoletosAncianos=0;
     BoletosAdultos=0;
     if (EntraPersona1Sala){
 
-        if (edad2> edad3){cout<< "La tercera persona va a acompañar al menor de edad"<< endl;}
-        else{cout<<"La segunda persona va a acompañar al menor de edad"<<endl;}}
+        if (edad2> edad3){cout<< "La tercera persona va a acompañar al menor de edad"<< endl; if(esAnciano2){BoletosAncianos+=1;}; if(EntraCine2){BoletosAdultos+=1;}}
+        else{cout<<"La segunda persona va a acompañar al menor de edad"<<endl;if(esAnciano3){BoletosAncianos+=1;}; if(EntraCine3){BoletosAdultos+=1;}}}
+    else if (EntraPersona2Sala){
 
-    else{cout << "Una de las dos personas adultas debe ir obligatoriamente con el menor de edad (dos boletos a la sala de entretenimiento) "<< endl;
-        BoletosMenores=2;
-        BoletosAdultos=1;}
-}
-else{
-if(esAnciano1){BoletosAncianos+=1;}else{BoletosAdultos+=1;};
-if(esAnciano2){BoletosAncianos+=1;}else{BoletosAdultos+=1;};
-if(esAnciano3){BoletosAncianos+=1;}else{BoletosAdultos+=1;};
+        if (edad3> edad1){cout<< "La primera persona va a acompañar al menor de edad"<< endl;if(esAnciano3){BoletosAncianos+=1;}; if(EntraCine3){BoletosAdultos+=1;}}
+        else{cout<<"La tercera persona va a acompañar al menor de edad"<<endl;if(esAnciano1){BoletosAncianos+=1;}; if(EntraCine1){BoletosAdultos+=1;}}}
+    else if (EntraPersona3Sala){
+
+        if (edad1> edad2){cout<< "La segunda persona va a acompañar al menor de edad"<< endl;if(esAnciano1){BoletosAncianos+=1;}; if(EntraCine1){BoletosAdultos+=1;}}
+        else{cout<<"La primera persona va a acompañar al menor de edad"<<endl;if(esAnciano2){BoletosAncianos+=1;}; if(EntraCine2){BoletosAdultos+=1;}}}
 }
 
 
