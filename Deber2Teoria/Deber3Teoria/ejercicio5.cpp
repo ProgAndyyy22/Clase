e9c743d0d35ca464181be84f5c1e3f03ce94ca6b
ProgAndyyy22 <asaraque@estud.usfq.edu.ec>
2026-09-23 15:51:48 -0500

    Update ejercicio5.cpp

--- Deber2Teoria/Deber3Teoria/ejercicio5.cpp
+++ Deber2Teoria/Deber3Teoria/ejercicio5.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main( ) {
 
 
-
+cout<< "Antropología \n" << endl;
 float PromedioConsultas{0};
 float NotaFinal{0};
 float factor=0.5;
@@ -16,8 +16,7 @@ cout << "Ingrese la nota de la segunda consulta"<< endl; cin>> nota2;
 cout << "Ingrese la nota del trabajo de medio semestre"<< endl; cin>> nota3;
 cout << "Ingrese la nota del proyecto final"<< endl; cin>> nota4;
 // el orden de las notas aparece de menor a mayor debido al for
-int i{2}; 
-for(i<=20; i++;){
+for(int i=2; i<=20; i++;){
 if(factor*i==nota1){existe1=true; cout<< "Consulta 1: "<< nota1<< endl;}
 if(factor*i==nota2){existe2=true; cout<< "Consulta 2: "<< nota2<< endl;}
 if(factor*i==nota3){existe3=true; cout<< "Trabajo de medio semestre: "<< nota3<< endl;}
@@ -26,7 +25,7 @@ if(factor*i==nota4){existe4=true; cout<< "Proyecto final: "<< nota4<< endl;}
 if(existe1==false){cout << "La nota de la primera consulta no se ingreso correctamente"<< endl;}
 if(existe2==false){cout << "La nota de la segunda consulta no se ingreso correctamente"<< endl;}
 if(existe3==false){cout << "La nota del trabajo de medio semestre no se ingreso correctamente"<< endl;}
-if(existe3==false){cout << "La nota del proyecto final no se ingreso correctamente"<< endl;}
+if(existe4==false){cout << "La nota del proyecto final no se ingreso correctamente"<< endl;}
 
 if(existe1 && existe2 && existe3 && existe4){
     PromedioConsultas=((nota1+nota2)/2.0);
